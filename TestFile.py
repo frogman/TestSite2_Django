ec0cc392b952bb1b1ee3c513404bbf971b26770d
frogman <frogman20xx@gmail.com>
2016-12-23 19:46:38 +0100

    test

--- TestFile.py
+++ TestFile.py
@@ -1,17 +1,5 @@
+from django.http import HttpResponse
 
-from django.conf import settings
-try:
-    import cProfile as profile
-except ImportError:
-    import profile
 
-def profile_tests(*args, **kwargs):
-    profile.runctx('run_tests(*args, **kwargs)',
-                {'run_tests':run_tests,'args':args,'kwargs':kwargs},
-                {},
-                getattr(settings,'TEST_PROFILE',None)
-            )
-
-# file: settings.py #
-TEST_RUNNER = 'profiling.profile_tests'
-TEST_PROFILE = None
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
